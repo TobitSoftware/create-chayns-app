0253635a718124e4d69e4a501a2150537b3d6d91
Heiner Pöpping <heiner.poepping@tobit.com>
2024-11-27 18:05:35 +0100

    Fix typescript error

--- templates/api-v5/page-ts-redux/src/index.tsx
+++ templates/api-v5/page-ts-redux/src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import AppWrapper from './components/AppWrapper';
 
 try {
-    const root = createRoot(document.querySelector('#root'));
+    const root = createRoot(document.querySelector('#root') as Element);
     root.render(<AppWrapper />);
 } catch (e) {
     console.error('Encountered error at `ReactDOM.render`: ', e);
