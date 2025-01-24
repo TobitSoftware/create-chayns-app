ebf095ec27d93fae831db794b41988f6dbfe6c66
Heiner Pöpping <heiner.poepping@tobit.software>
2025-01-24 18:28:03 +0100

    Update template for environment variables

--- templates/api-v5/shared/ts/src/types/environment.d.ts
+++ templates/api-v5/shared/ts/src/types/environment.d.ts
@@ -1,7 +1,7 @@
 declare namespace NodeJS {
     interface ProcessEnv {
-        PUBLIC_BUILD_ENV: string;
-        PUBLIC_BUILD_VERSION: string;
+        BUILD_ENV: string;
+        BUILD_VERSION: string;
 
         /* add custom env variables here */
         PUBLIC_YOUR_WEBAPI_URL: string;
