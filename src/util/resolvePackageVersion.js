54a85442d5bd72c071e48816f45e680b9e404028
Heiner Pöpping <heiner.poepping@tobit.com>
2025-02-04 09:59:41 +0100

    Ignore errors when resolving latest version

--- src/util/resolvePackageVersion.js
+++ src/util/resolvePackageVersion.js
@@ -1,17 +1,23 @@
 import { exec } from 'node:child_process';
+import chalk from 'chalk';
 
 export const resolvePackageVersion = async (pkg, tag) => {
     const target = tag ? `${pkg}@${tag}` : pkg;
-    const result = await new Promise((resolve, reject) => {
-        exec(`npm view ${target} version --json`, (error, out) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(out.trim());
-            }
+    try {
+        const result = await new Promise((resolve, reject) => {
+            exec(`npm view ${target} version --json`, (error, out) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(out.trim());
+                }
+            });
         });
-    });
-    const parsedResult = JSON.parse(result);
-    const version = Array.isArray(parsedResult) ? parsedResult.pop() : parsedResult;
-    return `^${version}`;
+        const parsedResult = JSON.parse(result);
+        const version = Array.isArray(parsedResult) ? parsedResult.pop() : parsedResult;
+        return `^${version}`;
+    } catch {
+        console.warn(`Could not resolve ${chalk.cyanBright(target)}`);
+        return tag || 'latest';
+    }
 };
