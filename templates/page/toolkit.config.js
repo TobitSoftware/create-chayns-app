c005a1387a0e89f48dd3e68725b5d14d63509f4d
Heiner Pöpping <heiner.poepping@tobit.software>
2025-02-07 19:56:31 +0100

    Add toolkit config file to v4 template

---
+++ templates/page/toolkit.config.js
@@ -0,0 +1,18 @@
+const { buildToolkitConfig } = require('chayns-toolkit');
+
+module.exports = buildToolkitConfig({
+    development: {
+        host: '0.0.0.0',
+        port: process.env.PORT || 8080,
+    },
+    output: {
+        entryPoints: {
+            index: {
+                pathIndex: './src/index',
+                pathHtml: './src/index.html',
+            },
+            // Add more entrypoints here
+        },
+        cssVersion: '4.2',
+    },
+});
