fa379930be21304af90cabcee4a552136c6a6e49
Heiner Pöpping <heiner.poepping@tobit.software>
2025-02-07 20:56:07 +0100

    Update v4 template to use createRoot

--- templates/page/src/index.jsx
+++ templates/page/src/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
-chayns.ready
-    .then(() => {
-        try {
-            ReactDOM.render(<App />, document.querySelector('#root'));
-        } catch (e) {
-            console.error('Encountered error at `ReactDOM.render`: ', e);
-        }
-    })
-    .catch((error) => {
-        console.warn('No chayns environment found.', error);
-    });
+try {
+    await chayns.ready;
+} catch (ex) {
+    console.warn('No chayns environment found.', ex);
+}
+
+try {
+    const root = createRoot(document.querySelector('#root'));
+    root.render(<App />);
+} catch (ex) {
+    console.error('Encountered error at `ReactDOM.render`: ', ex);
+}
