aec3d73d5badd8049f5c9d5c2158950555e157f3
Heiner Pöpping <heiner.poepping@tobit.com>
2026-07-13 11:13:15 +0200

    ✨ Add pre-typed createAsyncThunk utility

--- templates/api-v5/shared/ts/src/hooks/redux.ts
+++ templates/api-v5/shared/ts/src/hooks/redux.ts
@@ -1,5 +1,7 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from '../redux-modules';
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
+export const createAppAsyncThunk = createAsyncThunk.withTypes<{ dispatch: AppDispatch; state: RootState }>();
